f31ac2b94e983b7b1baa7ce9ad8252738d2730f8
Sunfox <egor.sharoha@gmail.com>
2017-09-15 17:34:24 +0600

    functional buy form

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,48 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    browsersync = require('browser-sync'),
+    gulp_jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    jshint = require('jshint');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var path = {
+    build : {
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/'
+    },
+    src : {
+        html: 'pages/**/*.html',
+        js: 'js/**/*.js',
+        css: 'css/*.css',
+        img: 'img/**/*.*'
+    },
+    clean : './build'
+};
+
+gulp.task('default', [
+    'html:build',
+    'js:build',
+    'img:build',
+    'css:build'
+]);
+
+gulp.task('html:build', function () {
+   gulp.src(path.src.html)
+       .pipe(gulp.dest(path.build.html))
+});
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(gulp.dest(path.build.js))
+});
+
+gulp.task('img:build', function () {
+    gulp.src(path.src.img)
+        .pipe(gulp.dest(path.build.img))
+});
+
+gulp.task('css:build', function () {
+    gulp.src(path.src.css)
+        .pipe(gulp.dest(path.build.css))
 });
