d3c472e681d6a50737d82ff9b652ca7986717445
Sunfox <egor.sharoha@gmail.com>
2017-09-23 23:35:14 +0600

    working authentication, full AJAX refactoring, more data collection

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var rubAccounts = ['Sberbank', 'Alfa-Bank', 'QIWI', 'Yandex.Money', 'QIWI-Yobit'
 var btcAccounts = ['BTC bot', 'BTC Yobit'];
 var botComission = 0.008;
 
+
 function fillSelect(select, data) {
     for (var j = select.options.length; j >= 0; j--) {
         select.remove(j);
@@ -24,6 +25,45 @@ function defineReservesSet(select_id) {
     } else return [];
 }
 
+function getFullPaymentData(callback) {
+    var totalRub = 0, totalBtc = 0, currentAvg = 0;
+    $.ajax({
+        url: apiServer + '/reportpay',
+        type: 'get',
+        headers: { 'authorization' : sessionStorage.getItem('token')},
+        crossDomain: true,
+        success: function (data) {
+            $.each(data, function (key, value) {
+                currentAvg = ((totalBtc * currentAvg) + value.rub)/(totalBtc + value.btc);
+                totalBtc += value.btc - value.bot_commiss;
+                totalRub -= value.rub + value.commiss;
+            });
+            var result = {
+                'data' : data,
+                'avg' : currentAvg.toFixed(2),
+                'btc' : totalBtc.toFixed(8),
+                'rub' : totalRub.toFixed(2)
+            };
+            callback(result);
+        }
+    });
+}
+
+function getFullSalesData(callback) {
+    $.ajax({
+        url: apiServer + '/reportsell',
+        type: 'get',
+        headers: { 'authorization' : sessionStorage.getItem('token')},
+        crossDomain: true,
+        success: function (data) {
+            var result = {
+                'data' : data
+            };
+            callback(result);
+        }
+    });
+}
+
 function getAvgCourse(callback) {
     var normal = 0, crypto = 0, avg_course = 0;
     $.getJSON(apiServer + '/reportpay', function (data) {
@@ -37,30 +77,76 @@ function getAvgCourse(callback) {
     });
 }
 
-function getTotalCryptoBought(callback) {
-    var result = 0;
-    $.getJSON(apiServer + '/reportpay', function (data) {
-        $.each(data, function (key, value) {
-            /*TODO include check for currencies; Right now it is counting everything as currency='BTC'*/
-            result += value.btc;
-        });
-        callback(result);
-    });
-}
-
-function getTotalNormalSpent(callback) {
-    var result = 0;
-    $.getJSON(apiServer + '/reportpay', function (data) {
-        $.each(data, function (key, value) {
-            /*TODO include check for currencies; Right now it is counting everything as currency='BTC'*/
-            result += value.rub;
-        });
-        callback(result);
-    });
-}
-
-function getDealFinrez(income, avg, btc, botcom) {
+function getDealFinrez(income, avgCourse, btcOutcome, botCommiss, comiss) {
     var result;
-    result = (income - (avg * btc) - (botcom * avg)).toFixed(2);
-    return result;
+    result = (income - (avgCourse * btcOutcome) - (botCommiss * avgCourse) - comiss).toFixed(2);
+    return parseFloat(result);
+}
+
+function getGlobalStatsFull(callback) {
+    var fullList, payList, sellList;
+    //pays
+    $.ajax({
+        url: apiServer + '/reportpay',
+        type: 'GET',
+        crossDomain: true,
+        headers: {
+            'authorization': sessionStorage.getItem('token')
+        },
+        success: function (data) {
+            payList = data;
+            $.each(payList, function (key, value) {
+                value.operation = 'pay'
+            });
+        }
+    }).done(function () {
+        //sells
+        $.ajax({
+            url: apiServer + "/reportSell",
+            crossDomain: true,
+            type: 'GET',
+            headers: {
+                'authorization': sessionStorage.getItem('token')
+            },
+            success: function (data) {
+                sellList = data;
+                $.each(sellList, function (key, value) {
+                   value.operation = 'sell';
+                });
+            }
+        }).done(function () {
+
+            //merging both lists and sorting result
+            fullList = payList.concat(sellList);
+            fullList.sort(function (a, b) {
+                return a.date > b.date ? 1 : -1;
+            });
+
+            //processing data
+            var totalFinrez = 0, totalRub = 0, totalBtc = 0, currentAvg = 0;
+            $.each(fullList, function (key, value) {
+                if (value.operation === "sell") {
+                    totalFinrez += getDealFinrez(value.rub, currentAvg, value.btc, value.bot_commiss, value.commiss);
+                    totalBtc -= value.btc + value.bot_commiss;
+                    totalRub += value.rub - value.commiss;
+                }
+                else if (value.operation === 'pay') {
+                    currentAvg = ((totalBtc * currentAvg) + value.rub)/(totalBtc + value.btc);
+                    totalBtc += value.btc - value.bot_commiss;
+                    totalRub -= value.rub + value.commiss;
+                    totalFinrez += getDealFinrez(0, currentAvg, 0, value.bot_commiss, value.commiss);
+                }
+            });
+
+            //sending data to the callback function
+            var result = {
+                'avgCourse' : currentAvg.toFixed(2),
+                'totalBtc' : totalBtc.toFixed(8),
+                'totalRub' : totalRub.toFixed(2),
+                'totalFinrez' : totalFinrez.toFixed(2)
+            };
+            callback(result);
+        });
+    });
+
 }
